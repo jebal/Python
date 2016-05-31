4d8895defcef936301715b90ab372da7a7feda39
jebal <jebal@126.com>
2016-05-31 10:29:44 +0800

    ebooksFetch
    增加电子书爬虫以及自动邮件发送

---
+++ EbooksFetchTool/read_conf.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+import ConfigParser
+import os
+
+
+if __name__ == '__main__':
+    cf = ConfigParser.ConfigParser()
+    cf.read("app.conf")
+
+    sender = cf.get("mail", "sender")
+    receivers = cf.get("mail", "receivers")
+
+    recv_list = []
+    [recv_list.append(str_recv) for str_recv in receivers]
+
+    print sender, receivers, recv_list
+    
